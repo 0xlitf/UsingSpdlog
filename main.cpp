625e8f5b59a2d3cec71eaa2da58558998338124c
0xlitf <0xlitf@gmail.com>
2024-01-08 17:03:35 +0800

    add example from spdlog

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
+void stdout_example()
+{
+    // create a color multi-threaded logger
+    auto console = spdlog::stdout_color_mt("console");
+    auto err_logger = spdlog::stderr_color_mt("stderr");
+    spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
+}
 
 int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
+
+    stdout_example();
 
     return app.exec();
 }
